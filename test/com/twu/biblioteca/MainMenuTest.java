cc866b9b5d14c6fc76d6f7210b18ca7412ff9aa3
karanssidhu96 <karanssidhu96@gmail.com>
2018-09-03 21:59:03 +0100

    Created main menu test class

---
+++ test/com/twu/biblioteca/MainMenuTest.java
@@ -0,0 +1,13 @@
+package com.twu.biblioteca;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class MainMenuTest {
+    @Test
+    public void mainMenuTest() {
+        MainMenu menu = new MainMenu();
+
+    }
+}
