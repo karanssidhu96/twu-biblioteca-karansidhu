d5223192fa75809f5358a799aac5f813b0342ab6
karanssidhu96 <karanssidhu96@gmail.com>
2018-09-05 15:54:30 +0100

    Green phase main menu unit test

--- src/com/twu/biblioteca/MainMenu.java
+++ src/com/twu/biblioteca/MainMenu.java
@@ -1,4 +1,33 @@
 package com.twu.biblioteca;
 
+import java.util.Scanner;
+
 public class MainMenu {
+    public MainMenu(Books books)
+    {
+        menu(books);
+    }
+
+    private void menu(Books books)
+    {
+        Scanner s = new Scanner(System.in);
+        System.out.println("Choose one of the following options");
+        System.out.println("List Books");
+        System.out.print("Your selection: ");
+        String selectedMenuItem = s.nextLine();
+        innerMenu(selectedMenuItem, books);
+    }
+
+    private void innerMenu(String selectedMenuItem, Books books)
+    {
+        switch (selectedMenuItem)
+        {
+            case "List Books":
+                books.printListAllBooks();
+                break;
+            default:
+                books.printListAllBooks();
+                break;
+        }
+    }
 }
--- test/com/twu/biblioteca/MainMenuTest.java
+++ test/com/twu/biblioteca/MainMenuTest.java
@@ -1,13 +1,39 @@
 package com.twu.biblioteca;
 
+import com.sun.corba.se.pept.encoding.InputObject;
+import org.junit.Before;
 import org.junit.Test;
 
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.InputStream;
+import java.io.PrintStream;
+
 import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 
 public class MainMenuTest {
+    private PrintStream sysOut;
+    private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+    @Before
+    public void setUpStreams(){
+        sysOut = System.out;
+        System.setOut(new PrintStream(outContent));
+    }
+
     @Test
     public void mainMenuTest() {
-        MainMenu menu = new MainMenu();
+        Books books = mock(Books.class);
+        String input = "List Books";
+        InputStream in = new ByteArrayInputStream(input.getBytes());
+        System.setIn(in);
+        String expectedResult = "Choose one of the following options\r\nList Books\r\nYour selection: ";
 
+        MainMenu menu = new MainMenu(books);
+        assertEquals(expectedResult, outContent.toString());
+
+        verify(books, times(1)).printListAllBooks();
     }
 }
